dd300028e3fb5a1b6f12124fb1a82e07b6410613
Deon Moolman <deon@stakara.com>
2024-08-21 15:26:07 +0200

    commit dist, because.

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+export { linkedErrorsIntegration, requestDataIntegration, dedupeIntegration, extraErrorDataIntegration, rewriteFramesIntegration, sessionTimingIntegration, } from './integrations';
+export type { LinkedErrorsOptions, RequestDataOptions } from './integrations';
+export { Toucan } from './sdk';
+export type { Options } from './types';
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/integrations/index.d.ts
@@ -0,0 +1,4 @@
+export * from './linkedErrors';
+export * from './requestData';
+export { dedupeIntegration, extraErrorDataIntegration, rewriteFramesIntegration, sessionTimingIntegration, } from '@sentry/core';
+//# sourceMappingURL=index.d.ts.map
